e3c3759e4f7e64edeba5cef08bea39264db5ab40
EnchantedTT <lcj199134@gmail.com>
2016-10-14 13:41:56 +0800

    complete python sort

---
+++ python/sort/main.py
@@ -0,0 +1,47 @@
+from Quicksort import Quicksort
+
+def is_sorted(numbers):
+    last_num = float("-inf")
+
+    for num in numbers:
+        if num < last_num:
+            return False
+        else:
+            last_num = num
+
+    return True
+
+def contain_same_ints(arr1, arr2):
+    for i in arr1:
+        found = False
+        for j in arr2:
+            if i == j:
+                found = True
+        if not found:
+            return False
+
+    return True
+
+def main():
+    original = [11, 2131, -123, 65, -35, 657, -2434]
+
+    numbers = original[:]
+
+    qs = Quicksort(numbers)
+    output = qs.sort()
+
+    if is_sorted(output):
+        print("** SUCCESS! **")
+    else:
+        print("Uh oh - not in order.")
+
+    if contain_same_ints(original, numbers):
+        print("** Contain the same elements! **")
+    else:
+        print("Uh oh - something is missing.")
+
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
--- python/sort/quicksort.py
+++ python/sort/quicksort.py
@@ -1,4 +1,39 @@
 import random
 
-class Quciksort(object):
-	
+class Quicksort(object):
+	def __init__(self, numbers):
+		self.values = numbers
+		self.count = len(self.values)
+
+	def sort(self):
+		self.quick(0, self.count - 1)
+		return self.values
+
+	def quick(self, left, right):
+		if left == right:
+			return
+
+		i = left
+		j = right
+
+		point_index = random.randint(left, right)
+
+		point = self.values[point_index]
+
+		while i <= j:
+			while self.values[i] < point:
+				i += 1
+			while self.values[j] > point:
+				j -= 1
+			if i <= j:
+				if i < j:
+					tmp = self.values[i]
+					self.values[i] = self.values[j]
+					self.values[j] = tmp
+				i += 1
+				j -= 1
+
+		if left < j:
+			self.quick(left, j)
+		if right > i:
+			self.quick(i, right)
