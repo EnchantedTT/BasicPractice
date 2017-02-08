493c94507a0f2875d031cd7c135bc6223a1ca85c
EnchantedTT <lcj199134@gmail.com>
2017-02-08 13:51:59 +0800

    sort

---
+++ algorithms/sort.py
@@ -0,0 +1,40 @@
+#O(nlogn)
+def quicksort(a):
+	if len(a) < 2:
+		return a
+	pivot = a[0]
+	a = a[1:]
+	left = [i for i in a if i <= pivot]
+	right = [i for i in a if i > pivot]
+	return quicksort(left) + [pivot] + quicksort(right)
+
+def heapsort(a):
+	pass
+
+def mergesort(a):
+	pass
+
+def insertsort(a):
+	pass
+
+#O(n**2)
+def bubblesort(a):
+	for i in xrange(len(a) - 1, 0, -1):
+		for j in xrange(0, i):
+			if a[j] > a[j + 1]:
+				a[j], a[j + 1] = a[j + 1], a[j]
+				flag = True
+				# tmp = a[j]
+				# a[j] = a[j + 1]
+				# a[j + 1] = tmp
+	return a
+
+def sort(a):
+	print quicksort(a)
+	print heapsort(a)
+	print mergesort(a)
+	print bubblesort(a)
+
+if __name__ == "__main__":
+	a = [100,21,42,52,231,66,32,51,199]
+	sort(a)
